123b56f05fd0314bea6a68501b0158850d2f7849
neongm <chura2013c@gmail.com>
2020-02-22 18:01:56 +0500

    more tasks

---
+++ v_18_lab/1_45_v2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+ 
+int main(){
+    int dates[7];
+    int out;
+    std::string answers[3] = {"первая дата идёт первой", "вторая дата идёт первой","даты одинаковы"};
+    std::cout<<"введите последовательно даты в формате: год1, месяц1, день1, год2, месяц2, день2"<<std::endl;
+    for(int i=0; i<6; i++) std::cin>>dates[i];
+    dates[6] = (dates[2]+dates[1]*12+dates[0]*365) - (dates[5]+dates[4]*12+dates[3]*365);
+    if (dates[6]) out = dates[6]>0 ? 1:0; else out = 2;
+    std::cout<<answers[out]<<std::endl;  
+    return 0;
+}
