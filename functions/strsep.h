b4afef7d0aa6b71ee27318562e04f311935cfd13
neongm <chura2013c@gmail.com>
2020-02-22 14:35:52 +0500

    empty separator fix

--- functions/strsep.h
+++ functions/strsep.h
@@ -1,10 +1,13 @@
-std::vector<std::string> separate(std::string &s, std::string sep=" "){
+std::vector<std::string> separate(std::string const &s, std::string sep=" "){
     int ind_begin, ind2_end = 0;
     std::vector<std::string> vec;
-    do{
-        ind2_end = s.find(sep, ind_begin);
-        vec.push_back(s.substr(ind_begin, ind2_end-ind_begin));
-        ind_begin = ind2_end+sep.size();
-    }while(ind2_end!=-1);
+    if(sep.size()==0) for(int i=0; i<s.size(); i++) vec.push_back(s.substr(i,1));
+    else{
+        do{
+            ind2_end = s.find(sep, ind_begin);
+            vec.push_back(s.substr(ind_begin, ind2_end-ind_begin));
+            ind_begin = ind2_end+sep.size();
+        }while(ind2_end!=-1);
+    }
     return vec;
-}                    //not working with empty line in separator
+}
--- sep.cpp
+++ sep.cpp
@@ -11,21 +11,24 @@ void vecout(std::vector<std::string> vec){
     std::cout<<']';
 }
 
-std::vector<std::string> separate(std::string &s, std::string sep=" "){
+std::vector<std::string> separate(std::string const &s, std::string sep=" "){
     int ind_begin, ind2_end = 0;
     std::vector<std::string> vec;
-    do{
-        ind2_end = s.find(sep, ind_begin);
-        vec.push_back(s.substr(ind_begin, ind2_end-ind_begin));
-        ind_begin = ind2_end+sep.size();
-    }while(ind2_end!=-1);
+    if(sep.size()==0) for(int i=0; i<s.size(); i++) vec.push_back(s.substr(i,1));
+    else{
+        do{
+            ind2_end = s.find(sep, ind_begin);
+            vec.push_back(s.substr(ind_begin, ind2_end-ind_begin));
+            ind_begin = ind2_end+sep.size();
+        }while(ind2_end!=-1);
+    }
     return vec;
-}                    //not working with empty line in separator
+}                    // empty separator fix
 
 int main(){
-    std::string test("DS FD SD");
-    std::cout<<"test string: "<<test<<"   separator: ' '"<<std::endl;
-    std::vector<std::string> vec = separate(test," ");
+    std::string test("DS  FD SD");
+    std::cout<<"test string: "<<test<<"   separator: ''"<<std::endl;
+    std::vector<std::string> vec = separate(test,"");
     vecout(vec);
     return 0;
 }
