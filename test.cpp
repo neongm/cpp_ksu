2b195be2c0eef4329d001ef83ed4136c0a994ad8
neongm <chura2013c@gmail.com>
2020-02-25 12:57:33 +0500

    fix

--- test.cpp
+++ test.cpp
@@ -8,13 +8,13 @@ int main(){
     std::string st;
     std::cout<<" sep is /,enter string:\n>";
     std::cin>>st;
-    std::cout<<" entered string:\n"<<st<<"\nseparated vector: ";
+    std::cout<<" entered string:\n"<<st<<"\nseparated vector: \n";
     std::vector<std::string> vec = separate(st, "/"); 
     auto intvec = vectoint(vec);
     vecout(vec);
     std::cout<<"\n integrals:\n";
     vecout(intvec);
-    std::cout<<"\n multiplied:\n";
+    std::cout<<"\n multiplied integrals:\n";
     for(int i=0; i<intvec.size(); i++) intvec.at(i)*=2;
-    for(int i=0; i<intvec.size(); i++) std::cout<<intvec.at(i)*2<<' ';
+    vecout(intvec);
 }
