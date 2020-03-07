653c9ac2b4e21fe835ffe5ccf2a180f2c16c4328
neongm <chura2013c@gmail.com>
2020-03-07 11:03:36 +0500

    update

--- v_18_lab/2_11.cpp
+++ v_18_lab/2_11.cpp
@@ -2,7 +2,7 @@
 #include "randint.h"
 
 int main(){
-    int N = 10;
+    int N = 5;
     int (*a)[2];
     a = new int[N][2];
     for(int i=0; i<N; i++){
@@ -12,13 +12,19 @@ int main(){
     for(int i=0; i<N; i++){
         std::cout<<i+1<<"-я точка: x="<<a[i][0]<<" y="<<a[i][1]<<'\n';
     }
-    int s_max=0, s_min=20, temp=0;  
+    int s_max=0, s_min=20, temp=0, max_index=0, min_index=0;  
     for(int i=0; i<N; i++){
         temp=a[i][0]+a[i][1];
         s_max = temp>s_max ? temp:s_max;   // самая правая верхняя через сумму
-        if(temp>s_max)
-        s_min = temp<s_min ? temp:s_min;   // самая левавя нижняя
+        if(temp>s_max) {
+            s_max = temp;
+            max_index = i;
+        }                     // todo
+        else if(temp<s_min) {
+            s_min = temp;
+            min_index = i;
+        }
     }
-    std::cout<<'\n'<<s_max;
-    std::cout<<'\n'<<s_min;
+    std::cout<<'\n'<<s_max<<' '<<max_index;
+    std::cout<<'\n'<<s_min<<' '<<min_index;
 }
