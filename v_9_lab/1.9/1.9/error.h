3cdf63ec811040dcf16a8d2ea97d2e5a70058bbc
̿|̿ ̿ |̶ ̶ ̶ ̶| |̶͇̿ ̶͇̿ ͇̿ Genix <delord001@gmail.com>
2020-03-12 11:03:13 +0500

    best forms app ever

---
+++ v_9_lab/1.9/1.9/error.h
@@ -0,0 +1,65 @@
+#pragma once
+
+namespace My19 {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// —водка дл€ error
+	/// </summary>
+	public ref class error : public System::Windows::Forms::Form
+	{
+	public:
+		error(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: добавьте код конструктора
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// ќсвободить все используемые ресурсы.
+		/// </summary>
+		~error()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+
+	private:
+		/// <summary>
+		/// “ребуетс€ переменна€ конструктора.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// ќб€зательный метод дл€ поддержки конструктора - не измен€йте
+		/// содержимое данного метода при помощи редактора кода.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->SuspendLayout();
+			// 
+			// error
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(282, 119);
+			this->Name = L"error";
+			this->Text = L"error";
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	};
+}
